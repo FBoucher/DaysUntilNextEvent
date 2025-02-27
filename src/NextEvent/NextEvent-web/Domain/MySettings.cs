706f8e987de8ee46391235be0d0b4ce202ec9e43
Frank <fboucher@outlook.com>
2025-02-27 07:43:13 -0500

    UI support new properties
    null values in config
    
    empty value

--- src/NextEvent/NextEvent-web/Domain/MySettings.cs
+++ src/NextEvent/NextEvent-web/Domain/MySettings.cs
@@ -1,14 +1,18 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace NextEvent_web.Domain;
 
 public class MySettings
 {
-    public string ImportantDate { get; set; } = "2025-08-19";
+    public string ImportantDate { get; set; } = DateTime.Now.ToString("yyyy-MM-dd");
+    public string StartFromDay { get; set; } = DateTime.Now.AddDays(-24).ToString("yyyy-MM-dd");
 
     public string PrimaryRGBColor { get; set; } = string.Empty;
 
-    public  string SecondaryRGBColor { get; set; } = string.Empty;
+    public string SecondaryRGBColor { get; set; } = string.Empty;
 
     public bool UseCustomColors { get; set; } = false;
+    public string StartTime { get; set; } = "09:00";
+    public string EndTime { get; set; } = "22:00";
 }
--- src/python/config.py
+++ src/python/config.py
@@ -1,4 +1,4 @@
-SSID = 'frankyboy'
-PASSWORD = '5144537131'
-SETTINGSURL = "https://storagec4ut4vi4benae.blob.core.windows.net/fileuploads/mysettings.json"
-PIXELS = 50
+SSID = ''
+PASSWORD = ''
+SETTINGSURL = ""
+PIXELS = 320
